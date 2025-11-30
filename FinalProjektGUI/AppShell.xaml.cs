4bb4831f5f58620f2b041f0054d18058ea831796
Dos12p <levi.puritch@gmail.com>
2025-11-30 03:49:40 -0800

    Creation

---
+++ FinalProjektGUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProjektGUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FinalProjektGUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FinalProjektGUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
